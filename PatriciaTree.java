8ee1cae3a12f97f87557ad4ed32182671944f1cf
WesleyMiranda <wesleywmm@hotmail.com>
2023-06-13 15:20:41 -0300

    Getters e Setters Criado

--- PatriciaTree.java
+++ PatriciaTree.java
@@ -1,4 +1,20 @@
 public class PatriciaTree {
     
+    No raiz;
+
+    public PatriciaTree() 
+    {
+        raiz = null;
+    }
+
+    public No getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(No raiz) {
+        this.raiz = raiz;
+    }
+
+    
 
 }
